08ef948492e68fb5f3aaa5dc15835e92abf55c09
abdikasa <abdikasa@sheridancollege.ca>
2020-08-30 16:52:54 -0400

    incorporated useEffect() to update the state with axios and renders the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App({ notes }) {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((res) => {
+      console.log(res);
+      console.log("promise fulfilled");
+      setNotes(res.data);
+    });
+  }, []);
+  console.log("render", notes.length, "notes");
   return (
     <div>
+      <h1>Notes</h1>
       <ul>
         {notes.map((note) => (
           <li key={note.id}>{note.content}</li>
@@ -10,6 +25,6 @@ function App({ notes }) {
       </ul>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import axios from "axios";
 
-const promise = axios.get("http://localhost:3001/notes");
-promise.then((response) => {
-  ReactDOM.render(
-    <App notes={response.data} />,
-    document.getElementById("root")
-  );
-});
+ReactDOM.render(<App />, document.getElementById("root"));
