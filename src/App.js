d477a16bbc86370f0d206ddc1a87a8bdd82b57de
abdikasa <abdikasa@sheridancollege.ca>
2020-08-30 22:54:00 -0400

    In progress: exercise 2.12, fetchd endpoint and displays data

--- src/App.js
+++ src/App.js
@@ -2,27 +2,34 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true);
+  const [filterValue, setFilter] = useState("");
+  const [countries, saveCountries] = useState([]);
 
-  useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((res) => {
-      console.log(res);
-      console.log("promise fulfilled");
-      setNotes(res.data);
+  const hook = () => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((res) => {
+      saveCountries(res.data);
+      console.log(res.data);
     });
-  }, []);
-  console.log("render", notes.length, "notes");
+  };
+
+  useEffect(hook, []);
+
+  const filteredResults =
+    filterValue.trim().length === 0
+      ? [{ name: "no data is found", numericCode: "9999" }]
+      : countries.filter((c) => c.name.includes(filterValue));
+
+  const inputChange = (event) => setFilter(event.target.value);
+
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
-        ))}
-      </ul>
+      filter countries here:{" "}
+      <input value={filterValue} onChange={inputChange} />
+      <div>
+        {filteredResults.map((country) => {
+          return <li key={country.numericCode}>{country.name}</li>;
+        })}
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// 2.12* Data for countries, step1 --- In progress
+// The API https://restcountries.eu provides a data for different countries in a machine readable format, a so-called REST API.
+// Create an application, in which one can look at data of various countries. The application should probably get the data from the endpoint all.
+// The user interface is very simple. The country to be shown is found by typing a search query into the search field.
+// If there are too many (over 10) countries that match the query, then the user is prompted to make their query more specific:
+
 ReactDOM.render(<App />, document.getElementById("root"));
