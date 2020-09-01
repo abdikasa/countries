36772930a7971d81030e5222f9d9880cb2bf4f4c
abdikasa <abdikasa@sheridancollege.ca>
2020-09-01 18:25:36 -0400

    Finished exercise 2.13

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from "./App";
 // The user interface is very simple. The country to be shown is found by typing a search query into the search field.
 // If there are too many (over 10) countries that match the query, then the user is prompted to make their query more specific:
 
-// 2.13*: Data for countries, step2 ---Starting
+// 2.13*: Data for countries, step2 ---FINNISHHED
 // Improve on the application in the previous exercise, such that when the names of multiple countries are shown on the page there is a button next to the name of the country, which when pressed shows the view for that country:
 
 ReactDOM.render(<App />, document.getElementById("root"));
