49fe8ca9466d499a82f6cbbc4628eaa32f87aa2c
abdikasa <abdikasa@sheridancollege.ca>
2020-08-31 20:36:42 -0400

    In progress 2.12: Updated Country.Js to show different use cases

---
+++ src/components/Country.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Country = ({ country }) => {
+  //case 1
+  //If there are too many (over 10) countries that match the query, then the user is prompted to make their query more specific:
+  //['Too many matches, specify another filter']
+
+  //case 2 --> More than 1 and less than 11.
+  //show country names ---> name property
+
+  //case 3 --- only 1 country
+  //h1 -> country name
+  //p -> capital, population
+  //h2 -> languages, ul <li>Languages<li>
+  //Languages is found in this array --> [{name}, {name}, {name}]
+
+  return <li key={country.numericCode}>{country.name}</li>;
+};
+
+export default Country;
